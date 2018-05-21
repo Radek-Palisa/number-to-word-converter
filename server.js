dd8b79033e5e60755a10f5bc48e14208fd032dc1
Radek Palisa <rpalisa@ebay.com>
2018-05-21 23:14:31 +0200

    adds morgan for console logging requests

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
 const apiRoutes = require('./api/routes/routes')
 
 const app = express()
 const port = process.env.PORT || 5000
+
+// requests logging into console
+app.use(morgan('dev'))
 
 // api routes
 app.use('/api', apiRoutes)
