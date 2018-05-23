257b2b26bd2aeaea9d637cab27b05888c644b274
Radek Palisa <rpalisa@ebay.com>
2018-05-23 22:20:45 +0200

    refining validation to block 0 and 1 to be passed in

--- api/middleware/validate.js
+++ api/middleware/validate.js
@@ -4,8 +4,8 @@ module.exports = {
 
         if (typeof payload !== 'string') {
             res.status(400).send({error: 'data submitted must be a string'})
-        } else if (!/^[0-9]+$/.test(payload)) {
-            res.status(400).send({error: 'data submitted must be a number string'})
+        } else if (!/^[2-9]+$/.test(payload)) {
+            res.status(400).send({error: 'data submitted must be a number from 2 to 9 corresponding to the T9 keyaboard'})
         } else {
             next()
         }
