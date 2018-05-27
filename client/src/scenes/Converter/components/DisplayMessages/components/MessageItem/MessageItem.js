6c185094dc36c7455593d51aa154bced24a37912
Radek Palisa <rpalisa@ebay.com>
2018-05-28 00:40:10 +0200

    fixing typos in MessageItem component

--- client/src/scenes/Converter/components/DisplayMessages/components/MessageItem/MessageItem.js
+++ client/src/scenes/Converter/components/DisplayMessages/components/MessageItem/MessageItem.js
@@ -4,9 +4,9 @@ import './MessageItem.css';
 class MessageItem extends Component {
     render() {
         return (
-            <li class={this.props.position === 'right' ?
+            <li className={this.props.position === 'right' ?
                 'messageItem messageItem--toRight' : 'messageItem'}>
-                <div class="messageItem__avatar"></div>
+                <div className="messageItem__avatar"></div>
                 <p className="messageItem__text">{this.props.text}</p>
             </li>
         );
